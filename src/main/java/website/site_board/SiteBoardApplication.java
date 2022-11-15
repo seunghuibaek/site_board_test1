bd8c23a34bd16c8c90aeafa65f0f304e653868ae
seunghuibaek <bluewar111@nate.com>
2022-11-15 20:52:54 +0900

    Initial commit

---
+++ src/main/java/website/site_board/SiteBoardApplication.java
@@ -0,0 +1,13 @@
+package website.site_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/website/site_board/SiteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package website.site_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
